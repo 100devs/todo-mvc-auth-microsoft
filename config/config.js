711ec2736dd039366f085e8735c1b8bb31db13a3
Ahmed Tarabia <ahmedtarabia@gmail.com>
2021-07-12 20:09:22 -0400

    proj

--- config/config.js
+++ config/config.js
@@ -1,47 +1,46 @@
 exports.creds = {
-    identityMetadata: 'https://login.microsoftonline.com/common/v2.0/.well-known/openid-configuration', 
+  identityMetadata: 'https://login.microsoftonline.com/common/v2.0/.well-known/openid-configuration', 
 
-    clientID: 'db7f56ff-e638-4dec-a220-7f710e087c88',
-  
-    clientSecret: 'vSWlkQc_.weq360un1c~fXvfKpM26_MT_9', 
-  
-    responseType: 'code id_token', 
-  
-    responseMode: 'form_post', 
-  
-    redirectUrl: 'http://localhost:3000/auth/openid/return', 
-  
-    allowHttpForRedirectUrl: true,
-  
-    validateIssuer: false,
-  
-    issuer: null,
-  
-    passReqToCallback: false,
-  
-    useCookieInsteadOfSession: false,
-  
-    cookieEncryptionKeys: [ 
-      { 'key': '12345678901234567890123456789012', 'iv': '123456789012' },
-      { 'key': 'abcdefghijklmnopqrstuvwxyzabcdef', 'iv': 'abcdefghijkl' }
-    ],
-  
-    scope: ['profile', 'offline_access', 'https://graph.microsoft.com/mail.read'],
-  
-    loggingLevel: false,
-  
-    nonceLifetime: null,
-  
-    nonceMaxAmount: 5,
-  
-    clockSkew: null,
-  };
-  
-  exports.destroySessionUrl = 'http://localhost:3000';
-  
-  exports.useMongoDBSessionStore = false;
-  
-  exports.databaseUri = 'mongodb://localhost/OIDCStrategy';
-  
-  exports.mongoDBSessionMaxAge = 24 * 60 * 60;  
-  
+  clientID: process.env.CLIENT_ID, 
+
+  clientSecret: process.env.CLIENT_SECRET, 
+
+  responseType: 'code id_token', 
+
+  responseMode: 'form_post', 
+
+  redirectUrl: (process.env.BASE_URL + '/auth/openid/return'), 
+
+  allowHttpForRedirectUrl: true,
+
+  validateIssuer: false,
+
+  issuer: null,
+
+  passReqToCallback: false,
+
+  useCookieInsteadOfSession: false,
+
+  cookieEncryptionKeys: [ 
+    { 'key': '12345678901234567890123456789012', 'iv': '123456789012' },
+    { 'key': 'abcdefghijklmnopqrstuvwxyzabcdef', 'iv': 'abcdefghijkl' }
+  ],
+
+  scope: ['profile', 'offline_access', 'https://graph.microsoft.com/mail.read'],
+
+  loggingLevel: false,
+
+  nonceLifetime: null,
+
+  nonceMaxAmount: 5,
+
+  clockSkew: null,
+};
+
+exports.destroySessionUrl = process.env.BASE_URL;
+
+exports.useMongoDBSessionStore = false;
+
+exports.databaseUri = 'mongodb://localhost/OIDCStrategy';
+
+exports.mongoDBSessionMaxAge = 24 * 60 * 60;
